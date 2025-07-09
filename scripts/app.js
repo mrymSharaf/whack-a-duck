02963f1598d606063096980dda5b907f5cb101f3
Mariam Sharaf <maryamshaider25@gmail.com>
2025-07-09 13:56:43 +0300

    add incrementing score

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,21 @@
 function init() {
 
     const gridElem = document.querySelector('.grid')
+    const scoreElem = document.querySelector('#score-display')
     const cells = []
     const gridWidth = 10
     const numOfCells = gridWidth * gridWidth
 
     let duckPosition = 10
-
+    let score = 0
+    
+    function clickDuck(event){
+        if(event.target.classList.contains('duck')){
+            score += 10
+            scoreElem.textContent = `Score ${score}`
+        }
+        
+    }
     function createGrid() {
         //for every cell we require create a div
         //append cell to our grid
@@ -15,8 +24,10 @@ function init() {
             const cellElem = document.createElement('div')
             // cellElem.classList.add('duck') 
             cellElem.textContent = i // display index 1,2...
+            cellElem.addEventListener('click', clickDuck)
             cells.push(cellElem) //add the cellElem into our cells array
-            gridElem.appendChild(cellElem) //
+            gridElem.appendChild(cellElem) 
+
         }
         console.log(cells)
     }
@@ -36,6 +47,7 @@ function init() {
             addDuck()
         }, 3000)
     }
+
     
     createGrid()
     play()
