2348736bd83ffdf93dd1eacc3bbabe9537d165bc
Mariam Sharaf <maryamshaider25@gmail.com>
2025-07-09 13:33:32 +0300

    update duck position

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,45 @@
-function init(){
+function init() {
 
     const gridElem = document.querySelector('.grid')
     const cells = []
     const gridWidth = 10
     const numOfCells = gridWidth * gridWidth
 
-    function createGrid(){
+    let duckPosition = 10
+
+    function createGrid() {
         //for every cell we require create a div
         //append cell to our grid
 
-        for(let i = 0; i < numOfCells; i++){
+        for (let i = 0; i < numOfCells; i++) {
             const cellElem = document.createElement('div')
-            cellElem.classList.add('duck')
+            // cellElem.classList.add('duck') 
             cellElem.textContent = i // display index 1,2...
             cells.push(cellElem) //add the cellElem into our cells array
             gridElem.appendChild(cellElem) //
         }
         console.log(cells)
     }
-        createGrid()
+
+    function addDuck() {
+        cells[duckPosition].classList.add('duck')
+    }
+
+    function removeDuck() {
+        cells[duckPosition].classList.remove('duck')
+    }
+
+    function play() {
+        setInterval(() => {
+            removeDuck()
+            duckPosition = Math.floor(Math.random() * numOfCells)
+            addDuck()
+        }, 3000)
+    }
+    
+    createGrid()
+    play()
+
 
 
 }
