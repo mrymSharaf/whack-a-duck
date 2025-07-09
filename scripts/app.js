e7af57ee790b671498fc7dfc88fc784f27caf683
Mariam Sharaf <maryamshaider25@gmail.com>
2025-07-09 11:14:35 +0300

    create grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,17 @@
 function init(){
 
-    
+    const gridElem = document.querySelector('.grid')
 
+    function createGrid(){
+        //for every cell we require create a div
+        //append cell to our grid
+
+        for(let i = 0; i < 100; i++){
+            const cellElem = document.createElement('div')
+            gridElem.appendChild(cellElem)
+            cellElem.textContent = i
+        }
+    }
+        createGrid()
 }
 document.addEventListener('DOMContentLoaded', init)
